becb85501e75685f87cd9c1dcde0ed5e527a5f43
izzatimahrup <n.izzatimahrup@gmail.com>
2025-11-01 21:34:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ import streamlit as st
 # Set up page configuration with Shopping Cart emoji as the icon
 st.set_page_config(
     page_title="Shopping Behaviour",  # Page title
-    page_icon="🛒"  # Shopping Cart emoji as the icon
+    page_icon="🛒", # Shopping Cart emoji as the icon
+    layout="wide"
 )
 
 # --- Define Pages with Navigation ---
 
-# Home page (🏠: House/Home is universally recognized)
+# Home page
 home = st.Page('home.py', title='HomePage', default=True, icon='🏠')
 
 # Page for Objective 1: Demographic Analysis
-# 👤: Bust/Silhouette is great for people/demographics.
 visualise_demographics = st.Page('shopping_behaviour.py', title='Demographic Analysis', icon='👤')
 
-# NEW PAGE 1 for Objective 2: Seasonality and Discounts
-# 🏷️: Tag is perfect for 'Discounts'.
+# Page for Objective 2: Seasonality and Discounts
 visualise_seasonality = st.Page('seasonality_discount.py', title='Season & Discount Analysis', icon='🏷️')
 
-# NEW PAGE 2 for Objective 3: Loyalty and Preferences
-# 👑: Crown/Gold Medal suggests 'Loyalty' or 'Premium status'.
+# Page for Objective 3: Loyalty and Preferences
 visualise_loyalty = st.Page('loyalty_preferences.py', title='Loyalty & Preferences', icon='👑')
 
 # --- Create a Navigation Structure ---
